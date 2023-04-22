d3607c049291a3a853ee108ff6581601b0f62e60
Csongor Zih <daemon.onwave@gmail.com>
2023-04-22 11:45:58 +0200

    Implement websocket

---
+++ islands/WebsocketClient.tsx
@@ -0,0 +1,47 @@
+import { StateUpdater, useState } from "preact/hooks";
+import { Button } from "../components/Button.tsx";
+
+enum ClientState {
+	CONNECTING,
+	CONNECTED,
+	DISCONNECTED,
+}
+
+const isServerSide = typeof location === "undefined";
+let ws: WebSocket | undefined;
+
+function connect(setState: StateUpdater<ClientState>) {
+	const url = new URL(location.href);
+	url.protocol = url.protocol.replace("http", "ws");
+	url.pathname = "/api/websocket";
+
+	ws = new WebSocket(url);
+	ws.onopen = () => {
+		console.log("connected");
+		setState(ClientState.CONNECTED);
+	}
+	ws.onmessage = (event) => {
+		console.log("message", event.data);
+	}
+	ws.onclose = () => {
+		console.log("disconnected");
+		setState(ClientState.DISCONNECTED);
+		ws = undefined;
+	}
+}
+
+function send(message: string) {
+	if (ws !== undefined) ws.send(message);
+}
+
+export default function WebsocketClient() {
+	const [state, setState] = useState(ClientState.CONNECTING);
+	if (!isServerSide) connect(setState);
+	return (
+		<div class="flex gap-2 w-full">
+			<p class="flex-grow-1 font-bold text-xl">{ClientState[state]}</p>
+			<input type="text" />
+			<Button onClick={() => send("hello")}>Send</Button>
+		</div>
+	);
+}
--- routes/api/websocket.ts
+++ routes/api/websocket.ts
@@ -1,6 +1,10 @@
 import { HandlerContext } from "https://deno.land/x/rutt@0.1.0/mod.ts";
 
 export const handler = (req: Request, _ctx: HandlerContext): Response => {
+	const upgrade = req.headers.get("upgrade") || "";
+	if (upgrade.toLowerCase() != "websocket") {
+	  return new Response("request isn't trying to upgrade to websocket.");
+	}
 	const { socket, response } = Deno.upgradeWebSocket(req);
 	socket.onmessage = (event) => {
 		socket.send(event.data);
